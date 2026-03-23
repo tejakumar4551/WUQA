d4e45f1a977831931ea4810d112ca84562e3e818
Teja Kumar Gundelugunta <tejakumar.g>
2026-03-23 13:07:01 +0530

    Removed all files from repo

--- config/env.js
+++
@@ -1,9 +0,0 @@
-export const ENV = {
-
-  BASE_URL: "https://fakestoreapi.com",
-  PRODUCTS_ENDPOINT: "/products",
-  LOGIN_ENDPOINT: "/auth/login",
-  USERNAME: "mor_2314",
-  PASSWORD: "83r5^_"
-
-};
